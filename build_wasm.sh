c3b714755f018c66b2b2f10546e52805f064ec15
Mustafa Quraish <mustafaq9@gmail.com>
2022-08-14 19:06:55 -0400

    Add basic collisions + score + text with SDL_ttf

--- build_wasm.sh
+++ build_wasm.sh
@@ -3,4 +3,4 @@
 set -e
 
 mkdir -p build
-emcc out.c -o build/index.html -sUSE_SDL_IMAGE=2 -sUSE_SDL=2 --preload-file images --use-preload-plugins
+emcc out.c -o build/index.html -sUSE_SDL_IMAGE=2 -sUSE_SDL_TTF=2 -sUSE_SDL=2 --preload-file assets --use-preload-plugins
